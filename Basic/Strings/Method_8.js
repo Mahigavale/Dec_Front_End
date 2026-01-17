440a04aa8d959d13fb7f27d40e7f2b8ed4e21b4b
Mahigavale <maheshgavale07@gmail.com>
2026-01-17 14:00:39 +0530

    Js

--- Basic/Strings/Method_8.js
+++ Basic/Strings/Method_8.js
@@ -2,15 +2,15 @@
 let str="abcdefghijk";
 
 
-   console.log(str.slice(0,3))
+//    console.log(str.slice(0,3))
    
-console.log(str.substring(9,-1));
+// console.log(str.substring(3,-2));
 
 /**
  * Sub str is not going to work with the negative number.
  */
-   console.log(str.substr(2,-1));
+  // console.log(str.substr(2,-1));
    
 
-   console.log(str);
+   //console.log(str);
    
